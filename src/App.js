10b584d7d00c81156f34ae8ef28bdee466206811
mrmicrowaveoven <benj@zagorski.com>
2022-12-23 11:14:27 -0800

    Added coming soon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p className="coming-soon-text">oSTEM NOLA: Coming soon!</p>
+        <img src="./fleur.jpeg" className="App-logo" alt="logo" />
       </header>
     </div>
   );
