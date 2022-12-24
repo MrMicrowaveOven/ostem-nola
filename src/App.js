271d06c4c18df44601e3f1e0b4fa82ef82168bd5
mrmicrowaveoven <benj@zagorski.com>
2022-12-23 17:02:34 -0800

    Added snazzy background animation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-filler">
         <p className="coming-soon-text">oSTEM NOLA: Coming soon!</p>
         <img src="./fleur.jpeg" className="App-logo" alt="logo" />
       </header>
