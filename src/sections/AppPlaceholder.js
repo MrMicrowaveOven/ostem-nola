9bb0658582b0855c0dba51f740349860a9264c8b
mrmicrowaveoven <benj@zagorski.com>
2022-12-25 11:54:31 -0800

    Added text

--- src/sections/AppPlaceholder.js
+++ src/sections/AppPlaceholder.js
@@ -1,6 +1,6 @@
 const AppPlaceholder = () =>
     <header className="app-filler">
-        <p className="coming-soon-text">oSTEM NOLA: Coming soon in 2023!</p>
+        <p className="coming-soon-text">oSTEM, New Orleans Chapter:<br/>Coming soon in 2023!</p>
         <img src="./fleur.jpeg" className="app-image spin" alt="logo" />
     </header>
 
