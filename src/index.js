ca30b4e4e0c1c097a4192b01768ab6be4e380243
mrmicrowaveoven <benj@zagorski.com>
2023-01-03 10:27:43 -0800

    Now removes annoying fb params from url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css'
-
+window.history.replaceState(null, null, window.location.pathname);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
