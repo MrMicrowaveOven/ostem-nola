2c0f3001875d66bd128989b9e08625a005b2ad4a
mrmicrowaveoven <benj@zagorski.com>
2023-01-03 11:21:16 -0800

    Added spin delay

--- src/sections/AppPlaceholder.js
+++ src/sections/AppPlaceholder.js
@@ -1,7 +1,14 @@
-const AppPlaceholder = () =>
-    <header className="app-filler">
-        <p className="coming-soon-text">oSTEM, New Orleans Chapter:<br/>Coming soon in 2023!</p>
-        <img src="./fleur.jpeg" className="app-image spin" alt="logo" />
-    </header>
+import { useState } from 'react'
+
+const AppPlaceholder = () => {
+    const [logoSpin, setLogoSpin] = useState(false)
+    setTimeout(() => setLogoSpin(true), 2000)
+    return (
+        <header className="app-filler">
+            <p className="coming-soon-text">oSTEM, New Orleans Chapter:<br/>Coming soon in 2023!</p>
+            <img src="./fleur.jpeg" className={`app-image ${logoSpin && "spin"}`} alt="logo" />
+        </header>
+    )
+}
 
 export default AppPlaceholder
